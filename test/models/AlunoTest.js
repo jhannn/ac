1f12b58585f9f4d7af5622314a9d364177347d21
Ruann Guerra <Ruann Guerra>
2017-02-24 15:03:32 -0300

    Teste de construtor de Aluno

---
+++ test/models/AlunoTest.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var should = require('should');
+
+var Aluno = require('../../app/models/Aluno.js');
+
+describe('# Teste da classe Aluno', function() {
+  describe('## Construtores', function() {
+      it('Aluno VALIDO', function() {
+        var aluno = new Aluno('20131014040048', 'Johann Guerra');
+
+        aluno.should.not.be.null;
+      });
+  });
+});
