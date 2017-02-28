d5e06f5e6ba496f68af1a4b3ce6d3cb51ef8c88d
Ruann Guerra <Ruann Guerra>
2017-02-28 06:36:43 -0300

    Teste do construtor de Professor

---
+++ test/models/ProfessorTest.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var should = require('should');
+
+var Professor = require('../../app/models/Professor.js');
+
+describe('# Teste da classe Professor', function() {
+  describe('## Construtores', function() {
+      it('Professor VALIDO', function() {
+        var professor = new Professor('54147', 'juninho');
+        professor.should.not.be.null;
+      });
+  });
+});
