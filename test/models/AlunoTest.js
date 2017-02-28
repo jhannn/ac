e91c35777db7500e15c716231cc9bbed4cd93498
Ruann Guerra <Ruann Guerra>
2017-02-28 06:32:55 -0300

    Retirada do metodo Setter

--- test/models/AlunoTest.js
+++ test/models/AlunoTest.js
@@ -6,17 +6,8 @@ var Aluno = require('../../app/models/Aluno.js');
 describe('# Teste da classe Aluno', function() {
   describe('## Construtores', function() {
       it('Aluno VALIDO', function() {
-        var aluno = new Aluno('20131014040048', 'Johann Guerra');
-
-        aluno.should.not.be.null;
-      });
-  });
-
-  describe('## Getter and Setter', function() {
-      it('Set Matricula', function() {
-        var aluno = new Aluno('20131014040048', 'Johann Guerra');
-        aluno.setMatricula('20131014040050');
-        aluno.should.have.property('matricula').be.eql('20131014040050');
+      	var aluno = new Aluno('20131014040048', 'Johann Guerra');
+		aluno.should.not.be.null;
       });
   });
 });
