ab628e557bd41c879c6629896236c9f25ac87b7d
Ruann Guerra <Ruann Guerra>
2017-03-01 11:57:47 -0300

    Feito o construtor de Requerimento

--- app/models/Requerimento.js
+++ app/models/Requerimento.js
@@ -1,8 +1,30 @@
+var GerenciadorRequerimentos = require('./GerenciadorRequerimentos.js');
+
 var Requerimento = function(aluno, tipo, disciplina, data) {
+	if(!verificarRequisitos){
+		return null;
+	}
 	this.aluno=aluno;
 	this.tipo=tipo;
 	this.disciplina=disciplina;
 	this.data=data;
+};
+
+function verificarRequisitos(aluno, tipo, disciplina, data){
+	var historico = aluno.historico;
+	if(historico.verificarRequisitoCH()){//Requisito de CargaHoraria
+		if(historico.verificarRequisitoDisciplina(disciplina)){
+			if(tipo=="certificacao"){
+				if(gerenciadorRequerimentos.verificarRequisitoTipoSemestre(aluno, data)){
+					return true;
+				}
+			}
+			else {
+				return true;
+			}
+		}
+	}
+	return false;
 };
 
 module.exports = Requerimento;
