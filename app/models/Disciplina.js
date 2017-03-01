70878203c2b22f51f7a182073dcdef6d8e4bf262
Ruann Guerra <Ruann Guerra>
2017-03-01 11:58:07 -0300

    Refatorado

--- app/models/Disciplina.js
+++ app/models/Disciplina.js
@@ -1,7 +1,7 @@
 var Disciplina = function(ano, nome, cargaHoraria, ementa, curso) {
 	this.ano=ano;
 	this.nome=nome;
-	this.cargaHoraria;
+	this.cargaHoraria=cargaHoraria;
 	this.ementa=ementa;
   	this.curso = curso;
 };
