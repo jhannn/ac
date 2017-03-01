c58a6f0518927f6bcc3bab8a846577d19b41ceeb
Ruann Guerra <Ruann Guerra>
2017-03-01 11:54:01 -0300

    Organizando os testes para a implementação do construtor correto

--- test/models/AlunoTest.js
+++ test/models/AlunoTest.js
@@ -2,11 +2,29 @@ var assert = require('assert');
 var should = require('should');
 
 var Aluno = require('../../app/models/Aluno.js');
+var Curso = require('../../app/models/Curso.js');
+var AlunoDisciplina = require('../../app/models/AlunoDisciplina.js');
+var Disciplina = require('../../app/models/Disciplina.js');
+var HistoricoAluno = require('../../app/models/HistoricoAluno.js');
+var Anexo = require('../../app/models/Anexo.js');
 
 describe('# Teste da classe Aluno', function() {
   describe('## Construtores', function() {
       it('Aluno VALIDO', function() {
-      	var aluno = new Aluno('20131014040048', 'Johann Guerra');
+      	var curso = new Curso('Tads', 2160);
+        var anexo = new Anexo('Ementa', 'Conteudo');
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Bd2', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
+        console.log(disciplinas);
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
+        console.log(alunoDisciplinas);
+      	var historico = new HistoricoAluno(alunoDisciplinas);
+      	var aluno = new Aluno('20131014040048', 'Johann Guerra', curso, historico);
 		aluno.should.not.be.null;
       });
   });
