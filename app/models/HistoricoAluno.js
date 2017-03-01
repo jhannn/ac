f7a60139ab791d2746316257c95f5ff8f9519783
Ruann Guerra <Ruann Guerra>
2017-03-01 11:56:54 -0300

    Refatorado

--- app/models/HistoricoAluno.js
+++ app/models/HistoricoAluno.js
@@ -1,5 +1,28 @@
-var HistoricoAluno = function(aluno) {
-	this.aluno=aluno;
+var HistoricoAluno = function(alunoDisciplina) {
+	this.alunoDisciplina=alunoDisciplina || [];
+};
+
+HistoricoAluno.prototype.verificarRequisitoCH=function(){
+	if(this.aluno.curso.cargaHoraria>this.getCH()){
+		return true;
+	}
+	return false;
+};
+
+HistoricoAluno.prototype.getCH=function(){
+	var totalCH=0;
+	for (var i in this.alunoDisciplina){
+		totalCH=totalCH+i.disciplina.cargaHoraria;
+	}
+	return totalCH;
+};
+
+HistoricoAluno.prototype.verificarRequisitoDisciplina=function(disciplina){
+	for (var i in this.alunoDisciplina){
+		if(i.disciplina==disciplina)
+			return false;
+	}
+	return true;
 };
 
 module.exports = HistoricoAluno;
