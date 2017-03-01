265841676e2e2acbe695adc9c986e4a1a350137e
Ruann Guerra <Ruann Guerra>
2017-03-01 11:56:25 -0300

    Refatorado

--- app/models/Aluno.js
+++ app/models/Aluno.js
@@ -1,6 +1,8 @@
-var Aluno = function(matricula, nome) {
+var Aluno = function(matricula, nome, curso, historico) {
   this.matricula = matricula;
   this.nome = nome;
+  this.curso = curso;
+  this.historico=historico;
 };
 
 module.exports = Aluno;
