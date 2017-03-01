db36eb2dd998e91f184d5fd422c3b5666cb24dd1
Ruann Guerra <Ruann Guerra>
2017-03-01 11:54:41 -0300

    Ajeitando uns bugs no test

--- test/models/HistoricoAlunoTest.js
+++ test/models/HistoricoAlunoTest.js
@@ -3,12 +3,27 @@ var should = require('should');
 
 var HistoricoAluno = require('../../app/models/HistoricoAluno.js');
 var Aluno = require('../../app/models/Aluno.js');
+var Curso = require('../../app/models/Curso.js');
+var Anexo = require('../../app/models/Anexo.js');
+var Disciplina = require('../../app/models/Disciplina.js');
+var AlunoDisciplina = require('../../app/models/AlunoDisciplina.js');
 
 describe('# Teste da classe Historico', function() {
   describe('## Construtores', function() {
       it('Historico VALIDO', function() {
-      	var aluno = new Aluno('20131014040048', 'Johann Guerra');
-      	var historico = new HistoricoAluno(aluno);
+      	var curso = new Curso('Tads', 2160);
+        var anexo = new Anexo('Ementa', 'Conteudo');
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Bd2', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
+        console.log(disciplinas);
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
+        console.log(alunoDisciplinas);
+      	var historico = new HistoricoAluno(alunoDisciplinas);
         historico.should.not.be.null;
       });
   });
