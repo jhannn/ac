7cc09748ba517c4c68311b28f2754cd016e0f153
Ruann Guerra <Ruann Guerra>
2017-03-01 11:55:43 -0300

    Ajeitado uns bugs

--- test/models/AlunoDisciplinaTest.js
+++ test/models/AlunoDisciplinaTest.js
@@ -3,7 +3,6 @@ var should = require('should');
 
 var AlunoDisciplina = require('../../app/models/AlunoDisciplina.js');
 var Curso = require('../../app/models/Curso.js');
-var Aluno = require('../../app/models/Aluno.js');
 var Disciplina = require('../../app/models/Disciplina.js');
 var Anexo = require('../../app/models/Anexo.js');
 
@@ -11,10 +10,9 @@ describe('# Teste da classe AlunoDisciplina', function() {
   describe('## Construtores', function() {
       it('AlunoDisciplina VALIDO', function() {
       	var curso = new Curso('Tads', 2160);
-		    var aluno = new Aluno('20131014040048', 'Johann Guerra');
         var anexo = new Anexo('Ementa', 'Conteudo');
         var disciplina = new Disciplina(2016, 'Bd1', 80, anexo, curso);
-        var alunoDisciplina = new AlunoDisciplina(aluno, disciplina, 'aprovado');
+        var alunoDisciplina = new AlunoDisciplina( disciplina, 'aprovado');
         alunoDisciplina.should.not.be.null;
       });
   });
