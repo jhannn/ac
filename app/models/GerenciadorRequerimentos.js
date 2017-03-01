b1ddacd408b72555c3110397902ca97c93668237
Ruann Guerra <Ruann Guerra>
2017-03-01 11:58:27 -0300

    Refatorado

--- app/models/GerenciadorRequerimentos.js
+++ app/models/GerenciadorRequerimentos.js
@@ -1,5 +1,19 @@
-var GerenciadorRequerimentos = function() {
+var GerenciadorRequerimentos = function(requerimentos) {
+	this.requerimentos=this.requerimentos || [];
+};
 
+var gerenciadorRequerimentos= new GerenciadorRequerimentos();
+
+GerenciadorRequerimentos.prototype.verificarRequisitoTipoSemestre=function(aluno, data){
+	var aux=[];
+	for (var i in this.requerimentos){
+		if(i.aluno==aluno && i.data.getTime()>(data.setMonth(data.getMonth() - 6)))
+			aux.push(i);
+	}
+	if(aux.length()>3){
+		return false;
+	}
+	return true;
 };
 
 module.exports = GerenciadorRequerimentos;
