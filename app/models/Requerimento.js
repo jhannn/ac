baf119fa67fa669b9d896aa16014bf47ca255b6d
Ruann Guerra <Ruann Guerra>
2017-03-01 22:42:14 -0300

    Refatorado a funcao verificarRequisitos

--- app/models/Requerimento.js
+++ app/models/Requerimento.js
@@ -1,16 +1,17 @@
 var GerenciadorRequerimentos = require('./GerenciadorRequerimentos.js');
 
-var Requerimento = function(aluno, tipo, disciplina, data) {
-	if(!verificarRequisitos){
+var Requerimento = function(aluno, tipo, disciplina, data, situacao) {
+	if(!this.verificarRequisitos(aluno, tipo, disciplina, data)){
 		return null;
 	}
 	this.aluno=aluno;
 	this.tipo=tipo;
 	this.disciplina=disciplina;
 	this.data=data;
+	this.situacao=situacao;
 };
 
-function verificarRequisitos(aluno, tipo, disciplina, data){
+Requerimento.prototype.verificarRequisitos = function(aluno, tipo, disciplina, data) {
 	var historico = aluno.historico;
 	if(historico.verificarRequisitoCH()){//Requisito de CargaHoraria
 		if(historico.verificarRequisitoDisciplina(disciplina)){
