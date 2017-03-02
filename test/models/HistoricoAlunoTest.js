f69d72184153d28b63798c45996b846ff0f2ff0d
Ruann Guerra <Ruann Guerra>
2017-03-01 22:41:06 -0300

    Adicionado Testes das funcoes de Historico

--- test/models/HistoricoAlunoTest.js
+++ test/models/HistoricoAlunoTest.js
@@ -17,14 +17,95 @@ describe('# Teste da classe Historico', function() {
         disciplinas.push(new Disciplina(2016, 'Bd1', 80, anexo, curso));
         disciplinas.push(new Disciplina(2016, 'Bd2', 80, anexo, curso));
         disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
-        console.log(disciplinas);
         var alunoDisciplinas = [];
         alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
         alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
         alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
-        console.log(alunoDisciplinas);
       	var historico = new HistoricoAluno(alunoDisciplinas);
         historico.should.not.be.null;
+      });
+  });
+
+  describe('## Funcoes', function() {
+      it('Verificar Requisito CH VALIDO', function() {
+        var curso = new Curso('Tads', 2160);
+        var anexo = new Anexo('Ementa', 'Conteudo');
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Bd2', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var requisitoCH=historico.verificarRequisitoCH();
+        requisitoCH.should.be.eql(true);
+      });
+
+      it('Verificar Requisito CH Invalida', function() {
+        var curso = new Curso('Tads', 2160);
+        var anexo = new Anexo('Ementa', 'Conteudo');
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 2000, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Bd2', 1000, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var requisitoCH=historico.verificarRequisitoCH();
+        requisitoCH.should.be.eql(false);
+      });
+
+      it('Verificar Total CH', function() {
+        var curso = new Curso('Tads', 2160);
+        var anexo = new Anexo('Ementa', 'Conteudo');
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Bd2', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var totalCH=historico.getCH();
+        totalCH.should.be.eql(240);
+      });
+
+      it('Verificar Requisito Disciplina Valida', function() {
+        var curso = new Curso('Tads', 2160);
+        var anexo = new Anexo('Ementa', 'Conteudo');
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Bd2', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
+        disciplinaASerTestada=new Disciplina(2016, 'Log', 80, anexo, curso);
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var requisitoDisc=historico.verificarRequisitoDisciplina(disciplinaASerTestada);
+        requisitoDisc.should.be.eql(true);
+      });
+
+      it('Verificar Requisito Disciplina Invalida', function() {
+        var curso = new Curso('Tads', 2160);
+        var anexo = new Anexo('Ementa', 'Conteudo');
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Bd2', 80, anexo, curso));
+        disciplinas.push(new Disciplina(2016, 'Prog', 80, anexo, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[0], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[1], 'aprovado'));
+        alunoDisciplinas.push(new AlunoDisciplina( disciplinas[2], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var requisitoDisc=historico.verificarRequisitoDisciplina(disciplinas[0]);
+        requisitoDisc.should.be.eql(false);
       });
   });
 });
