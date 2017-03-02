2f646f3744d10834748b3f5848bd62d7d2a66466
Ruann Guerra <Ruann Guerra>
2017-03-01 22:40:46 -0300

    Adicionado Testes das funcoes de Gerenciador

--- test/models/GerenciadorRequerimentosTest.js
+++ test/models/GerenciadorRequerimentosTest.js
@@ -7,18 +7,89 @@ var Aluno = require('../../app/models/Aluno.js');
 var Anexo = require('../../app/models/Anexo.js');
 var Disciplina = require('../../app/models/Disciplina.js');
 var Requerimento = require('../../app/models/Requerimento.js');
+var AlunoDisciplina = require('../../app/models/AlunoDisciplina.js');
+var HistoricoAluno = require('../../app/models/HistoricoAluno.js');
 
 describe('# Teste da classe GerenciadorRequerimentos', function() {
   describe('## Construtores', function() {
       it('Gerenciador de Requerimentos VALIDO', function() {
-      	var data = new Date();
-      	var aluno = new Aluno('20131014040048', 'Johann Guerra');
-      	var ementa = new Anexo('Ementa', 'Conteudo');
+        var data = new Date();
+        var ementa = new Anexo('Ementa', 'Conteudo');
         var curso = new Curso('Tads', 2160);
-      	var disciplina = new Disciplina(2016, 'Bd1', 80, ementa, curso);
-        var requerimento = new Requerimento(aluno, 'aproveitamento', disciplina, data);
-        var gerenciadorRequerimentos = new GerenciadorRequerimentos(requerimento);
-		gerenciadorRequerimentos.should.not.be.null;
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, ementa, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina(disciplinas[0], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var aluno = new Aluno('20131014040048', 'Johann Guerra', curso, historico);
+        var disciplinaParaRequerimento = new Disciplina(2016, 'Bd1', 80, ementa, curso);
+        var requerimentos = [];
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        var gerenciadorRequerimentos = new GerenciadorRequerimentos(requerimentos);
+		    gerenciadorRequerimentos.should.not.be.null;
+      });
+  });
+
+  describe('## Funcoes', function() {
+      it('Verificar Requisito Tipo Semestre VALIDO', function() {
+        var data = new Date();
+        var ementa = new Anexo('Ementa', 'Conteudo');
+        var curso = new Curso('Tads', 2160);
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, ementa, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina(disciplinas[0], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var aluno = new Aluno('20131014040048', 'Johann Guerra', curso, historico);
+        var disciplinaParaRequerimento = new Disciplina(2016, 'Bd1', 80, ementa, curso);
+        var requerimentos = [];
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        var gerenciadorRequerimentos = new GerenciadorRequerimentos(requerimentos);
+        var requisitoSemestre=gerenciadorRequerimentos.verificarRequisitoTipoSemestre(aluno, data);
+        requisitoSemestre.should.be.eql(true);
+      });
+
+      it('Verificar Requisito Tipo Semestre Invalida', function() {
+        var data = new Date();
+        var ementa = new Anexo('Ementa', 'Conteudo');
+        var curso = new Curso('Tads', 2160);
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, ementa, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina(disciplinas[0], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var aluno = new Aluno('20131014040048', 'Johann Guerra', curso, historico);
+        var disciplinaParaRequerimento = new Disciplina(2016, 'Bd1', 80, ementa, curso);
+        var requerimentos = [];
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        var gerenciadorRequerimentos = new GerenciadorRequerimentos(requerimentos);
+        var requisitoSemestre=gerenciadorRequerimentos.verificarRequisitoTipoSemestre(aluno, data);
+        requisitoSemestre.should.be.eql(false);
+      });
+
+      it('Verificar Listar Requerimentos em Aberto', function() {
+        var data = new Date();
+        var ementa = new Anexo('Ementa', 'Conteudo');
+        var curso = new Curso('Tads', 2160);
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, ementa, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina(disciplinas[0], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var aluno = new Aluno('20131014040048', 'Johann Guerra', curso, historico);
+        var disciplinaParaRequerimento = new Disciplina(2016, 'Bd1', 80, ementa, curso);
+        var requerimentos = [];
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        requerimentos.push(new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberto'));
+        var gerenciadorRequerimentos = new GerenciadorRequerimentos(requerimentos);
+        var listaDeRequisiscoesEmAberto=gerenciadorRequerimentos.listarRequerimentosEmAberto().length;
+        listaDeRequisiscoesEmAberto.should.be.eql(4);
       });
   });
 });
