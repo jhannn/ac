6e4e40160132443cfec38dec5747e4d5e4e4ac6f
Ruann Guerra <Ruann Guerra>
2017-03-01 22:43:19 -0300

    implementado as funcoes dos testes comitados anteriormente

--- app/models/GerenciadorRequerimentos.js
+++ app/models/GerenciadorRequerimentos.js
@@ -1,19 +1,35 @@
 var GerenciadorRequerimentos = function(requerimentos) {
-	this.requerimentos=this.requerimentos || [];
+	this.requerimentos=requerimentos || [];
 };
 
 var gerenciadorRequerimentos= new GerenciadorRequerimentos();
 
 GerenciadorRequerimentos.prototype.verificarRequisitoTipoSemestre=function(aluno, data){
 	var aux=[];
-	for (var i in this.requerimentos){
-		if(i.aluno==aluno && i.data.getTime()>(data.setMonth(data.getMonth() - 6)))
-			aux.push(i);
-	}
-	if(aux.length()>3){
-		return false;
+	var arrayAux=this.requerimentos;
+	if(arrayAux.length!=0){
+		for (i = 0; i < this.requerimentos.length; i++) { 
+			if(arrayAux[i].aluno==aluno && arrayAux[i].data.getTime()>(data.setMonth(data.getMonth() - 6)))
+				aux.push(arrayAux[i]);
+		}
+		if(aux.length>3){
+			return false;
+		}
 	}
 	return true;
+};
+
+GerenciadorRequerimentos.prototype.listarRequerimentosEmAberto = function() {
+	var aux=[];
+	var arrayAux=this.requerimentos;
+	if(arrayAux.length!=0){
+		for (i = 0; i < arrayAux.length; i++) { 
+			console.log(arrayAux[i]);
+			if(arrayAux[i].situacao=='aberto')
+				aux.push(arrayAux[i]);
+		}
+	}
+	return aux;
 };
 
 module.exports = GerenciadorRequerimentos;
