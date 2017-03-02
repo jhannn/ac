421bbb6c5e185aed6ff38d1dacce1cc7e3661a94
Ruann Guerra <Ruann Guerra>
2017-03-01 22:40:00 -0300

    Adicionado Historico ao aluno no test de Requerimento

--- test/models/RequimentoTest.js
+++ test/models/RequimentoTest.js
@@ -6,16 +6,23 @@ var Aluno = require('../../app/models/Aluno.js');
 var Disciplina = require('../../app/models/Disciplina.js');
 var Anexo = require('../../app/models/Anexo.js');
 var Curso = require('../../app/models/Curso.js');
+var AlunoDisciplina = require('../../app/models/AlunoDisciplina.js');
+var HistoricoAluno = require('../../app/models/HistoricoAluno.js');
 
 describe('# Teste da classe Requerimento', function() {
   describe('## Construtores', function() {
       it('Requerimento VALIDO', function() {
       	var data = new Date();
-      	var aluno = new Aluno('20131014040048', 'Johann Guerra');
       	var ementa = new Anexo('Ementa', 'Conteudo');
         var curso = new Curso('Tads', 2160);
-      	var disciplina = new Disciplina(2016, 'Bd1', 80, ementa, curso);
-        var requerimento = new Requerimento(aluno, 'aproveitamento', disciplina, data);
+        var disciplinas=[];
+        disciplinas.push(new Disciplina(2016, 'Bd1', 80, ementa, curso));
+        var alunoDisciplinas = [];
+        alunoDisciplinas.push(new AlunoDisciplina(disciplinas[0], 'aprovado'));
+        var historico = new HistoricoAluno(alunoDisciplinas);
+        var aluno = new Aluno('20131014040048', 'Johann Guerra', curso, historico);
+      	var disciplinaParaRequerimento = new Disciplina(2016, 'Bd1', 80, ementa, curso);
+        var requerimento = new Requerimento(aluno, 'aproveitamento', disciplinaParaRequerimento, data, 'aberta');
         requerimento.should.not.be.null;
       });
   });
